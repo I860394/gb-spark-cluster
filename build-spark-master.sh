00a4b1220fa31fbaaac4e040767cdef9e200b50d
Param Tatini <param.tatini@sap.com>
2017-12-21 10:53:48 -0800

    .

--- build-spark-master.sh
+++ build-spark-master.sh
@@ -10,4 +10,5 @@ docker network create --driver bridge gbcluster
 #   sudo service ssh restart
 #fi
 docker build -t 'spark-master-alpine:3.6' .
+docker rm -f spark-master
 docker run --rm --name spark-master --network=gbcluster -p7077:7077 -p8080:8080 -i -t spark-master-alpine:3.6
