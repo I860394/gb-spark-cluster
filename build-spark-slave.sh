0976949e95852da921f1d2089c10430942a12548
Param Tatini <param.tatini@sap.com>
2017-12-21 11:37:53 -0800

    .

--- build-spark-slave.sh
+++ build-spark-slave.sh
@@ -1,4 +1,4 @@
 cp Dockerfile-slave Dockerfile
 docker build -t 'spark-slave-alpine:3.6' .
-docker rm -f spark-slave
-docker run --rm --name spark-slave -p8081:8081 -i -t spark-slave-alpine:3.6
+#docker rm -f spark-slave
+docker run -v /home/vagrant/gb-spark-cluster:/home/vagrant/gb-spark-cluster --rm --name spark-slave -p8081:8081 -i -t spark-slave-alpine:3.6
